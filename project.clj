0bedf21043846c5395b2c06b6b90b00083142bbf
juanantonioruz <juanantonioruz@gmail.com>
2012-10-02 12:26:49 +0200

    clojure ring heroku example

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject ring1 "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring/ring-core "1.1.3"]
+                 [ring/ring-jetty-adapter "1.1.3"]])
+
---
+++ src/ring1/core.clj
@@ -0,0 +1,13 @@
+(ns ring1.core
+  (:use ring.adapter.jetty))
+
+
+
+  (defn handler [request]
+    {:status 200
+     :header {"Content-Type" "text/html"}
+     :body "primer ejemplo con  RING/Clojure!"})
+
+(defn -main[]
+  (let [port (Integer/parseInt (System/getenv "PORT"))]
+    run-jetty app {:port port}))
